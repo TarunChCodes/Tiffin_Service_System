de48f76967a8604f15117c2d6b2801f495f8c2bd
Tarun <chaudharyatrun031@gmail.com>
2023-05-25 22:36:25 +0530

    last commit of the Day

--- src/TiffinSerSys/TiffinCat.java
+++ src/TiffinSerSys/TiffinCat.java
@@ -1,10 +1,32 @@
 package TiffinSerSys;
 
-public class TiffinCat {
-    
+import java.awt.*;
+import javax.swing.*;
+
+public class TiffinCat extends JFrame {
+    String usrtype, id;
+
+    JMenuBar menuBar;
+    JMenu tiffinMenu, pacekedMenu;
 
     public TiffinCat(String usrtype, String id){
-       
+       this.usrtype = usrtype;
+       this.id = id;
+
+       JFrame TifcatFrame = new JFrame("Tiffin Cat");
+       TifcatFrame.setLayout(null);
+       TifcatFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+       TifcatFrame.setBounds(290,0,1000,725);
+
+        menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        tiffinMenu = new JMenu("Tiffin");
+        tiffinMenu.setFont(new Font("sherif",Font.PLAIN,16));
+        menuBar.add(tiffinMenu);
+
+
+        TifcatFrame.setVisible(true);
     }
 
     public static void main(String[] args) {
--- src/TiffinSerSys/TiffinCatAdmin.java
+++ src/TiffinSerSys/TiffinCatAdmin.java
@@ -2,4 +2,11 @@ package TiffinSerSys;
 
 public class TiffinCatAdmin {
     
+    public TiffinCatAdmin() {
+
+    }
+    
+    public static void main(String[] args) {
+        new TiffinCatAdmin();
+    }
 }
