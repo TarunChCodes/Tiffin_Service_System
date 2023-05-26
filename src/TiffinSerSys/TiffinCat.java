13db52a6d23ffdd8e4e085d0c4013e4868047574
Tarun <chaudharyatrun031@gmail.com>
2023-05-26 11:07:08 +0530

    last commit

--- src/TiffinSerSys/TiffinCat.java
+++ src/TiffinSerSys/TiffinCat.java
@@ -6,30 +6,16 @@ import javax.swing.*;
 public class TiffinCat extends JFrame {
     String usrtype, id;
 
-    JMenuBar menuBar;
-    JMenu tiffinMenu, pacekedMenu;
-
     public TiffinCat(String usrtype, String id){
        this.usrtype = usrtype;
        this.id = id;
 
        JFrame TifcatFrame = new JFrame("Tiffin Cat");
-       TifcatFrame.setLayout(new FlowLayout());
+       TifcatFrame.setLayout(null);
        TifcatFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
        TifcatFrame.setBounds(290,0,1000,725);
        TifcatFrame.setResizable(false);
        TifcatFrame.setBackground(Color.WHITE);
-
-        JMenuBar menubar = new JMenuBar();
-        setJMenuBar(menubar);
-
-        JMenu menu = new JMenu("Menu");
-        menu.setFont(new Font("sherif",Font.PLAIN,16));
-        menubar.add(menu);
-
-//        tiffinMenu = new JMenu("Tiffin");
-//        tiffinMenu.setFont(new Font("sherif",Font.PLAIN,16));
-//        menuBar.add(tiffinMenu);
 
 
         TifcatFrame.setVisible(true);
