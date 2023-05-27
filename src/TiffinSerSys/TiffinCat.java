a346f08177e61dee4f021d8bb3eee67cc38521f2
Tarun <chaudharyatrun031@gmail.com>
2023-05-27 11:08:28 +0530

    Last Commit

--- src/TiffinSerSys/TiffinCat.java
+++ src/TiffinSerSys/TiffinCat.java
@@ -1,9 +1,13 @@
 package TiffinSerSys;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
 
-public class TiffinCat extends JFrame {
+public class TiffinCat extends JFrame implements ActionListener {
     String usrtype, id;
     JFrame TifcatFrame;
 
@@ -18,15 +22,21 @@ public class TiffinCat extends JFrame {
         TifcatFrame = new JFrame("Tiffin Cat");
         TifcatFrame.setBounds(290,0,1000,725);
         TifcatFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        TifcatFrame.setLayout(new FlowLayout());
+        TifcatFrame.setLayout(null);
 
         menubar = new JMenuBar();
         TifcatFrame.setJMenuBar(menubar);
 
+//        ---------- Menu ------------
         tiffinMenu = new JMenu("Tiffins");
         tiffinMenu.setFont(new Font("sherif",Font.PLAIN,16));
         menubar.add(tiffinMenu);
 
+        packitemMenu = new JMenu("Packed Items");
+        packitemMenu.setFont(new Font("sherif",Font.PLAIN,16));
+        menubar.add(packitemMenu);
+
+//        ------------- MenuItems ------------
         vegTiffin = new JMenuItem("Veg Tiffin");
         vegTiffin.setFont(new Font("sherif",Font.PLAIN,14));
         tiffinMenu.add(vegTiffin);
@@ -35,9 +45,35 @@ public class TiffinCat extends JFrame {
         nonvegTiffin.setFont(new Font("sherif",Font.PLAIN,14));
         tiffinMenu.add(nonvegTiffin);
 
-        packitemMenu = new JMenu("Packed Items");
-        packitemMenu.setFont(new Font("sherif",Font.PLAIN,16));
-        menubar.add(packitemMenu);
+        JPanel vegPanel = new JPanel();
+        vegPanel.setSize(1000,725);
+        vegPanel.setBackground(Color.orange);
+        TifcatFrame.add(vegPanel);
+        vegPanel.setVisible(false);
+
+        JPanel nonvegPanel = new JPanel();
+        nonvegPanel.setSize(1000,725);
+        nonvegPanel.setBackground(Color.GREEN);
+        TifcatFrame.add(nonvegPanel);
+        nonvegPanel.setVisible(false);
+
+        vegTiffin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                vegPanel.setVisible(true);
+                nonvegPanel.setVisible(false);
+
+            }
+        });
+        nonvegTiffin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                nonvegPanel.setVisible(true);
+                vegPanel.setVisible(false);
+            }
+        });
+
+
 
 
         TifcatFrame.setVisible(true);
@@ -45,5 +81,10 @@ public class TiffinCat extends JFrame {
 
     public static void main(String[] args) {
         new TiffinCat("","");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
